4e77f9152744459fc1a2248d39e28b51caccae82
Deepak Yadav <121482719+DeepakYadav9892@users.noreply.github.com>
2025-07-22 21:16:34 +0530

    dfs_non_recursive1.a.py

---
+++ dfs_non_recursive1.a.py
@@ -0,0 +1,22 @@
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+def dfs_paths(graph, start, goal):
+    stack = [(start, [start])]
+    
+    while stack:
+        (vertex, path) = stack.pop()
+        
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
+
+print(list(dfs_paths(graph, 'A', 'F')))
